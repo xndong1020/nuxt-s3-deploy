9b9f7b27257cb0e573d1fd23b42367f210f38559
Jeremy Gu <jgu@networkten.com.au>
2019-11-20 13:46:14 +1100

    first template

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -7,10 +7,10 @@
         class="mb-5"
       >
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+        &#8220;This is a path from 'api-s3-proy'.&#8221;
         <footer>
           <small>
-            <em>&mdash;John Johnson</em>
+            <em>&mdash;api-s3-proy</em>
           </small>
         </footer>
       </blockquote>
